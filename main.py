2fb306d61f72100931ef5fe3d5690e15fdf07ae0
Briana <briana.yadjam@etu.imt-nord-europe.fr>
2026-04-17 00:20:49 +0200

    Implement Favorites Endpoints

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from typing import List, Annotated
 from sqlalchemy.orm import Session
 from database import SessionLocal
-from models import Pokemon
+from models import Pokemon, Favorites
 
 
 app = FastAPI()
@@ -12,6 +12,14 @@ class PokemonSchema(BaseModel):
     id: int
     name : str
     sprite_url: str
+
+class FavoritesSchema(BaseModel): 
+    id : int
+    pokemon_id : int
+
+
+class FavoriteCreate(BaseModel): 
+    pokemon_id : int
 
 def get_db():
     db = SessionLocal()
@@ -27,11 +35,47 @@ def get_db():
 @app.get("/pokemon", response_model=List[PokemonSchema])
 def get_pokemon(db: Session = Depends(get_db)):
     return db.query(Pokemon).all()
-#Get pokemon by id 
-@app.get("/pokemon/{id}", response_model=PokemonSchema)
-def get_pokemon_by_id(id: int, db: Session = Depends(get_db)):
-    return db.query(Pokemon).filter(Pokemon.id == id).first()
 #Get pokemon by name
-@app.get("/pokemon/{name}", response_model= PokemonSchema)
+@app.get("/pokemon/name/{name}", response_model= PokemonSchema)
 def get_pokemon_by_name(name: str, db: Session = Depends(get_db)):
     return db.query(Pokemon).filter(Pokemon.name == name).first()
+
+#Get pokemon by id 
+@app.get("/pokemon/id/{id}", response_model=PokemonSchema)
+def get_pokemon_by_id(id: int, db: Session = Depends(get_db)):
+    return db.query(Pokemon).filter(Pokemon.id == id).first()
+
+
+
+###API endpoints to retrive pokemon from database
+
+# API to get all the pokemon in the favorites
+@app.get("/favorites", response_model=List[PokemonSchema])
+def get_favorites(db:Session = Depends(get_db)):
+        favorites = db.query(Favorites).all()
+        pokemon_ids = [fav.pokemon_id for fav in favorites]
+        return db.query(Pokemon).filter(Pokemon.id.in_(pokemon_ids)).all()
+
+# Api to add a pokemon to the favorites
+@app.post("/favorites")
+def add_to_favorite(fav: FavoriteCreate, db: Session = Depends(get_db)):
+    existing = db.query(Favorites).filter(Favorites.pokemon_id == fav.pokemon_id).first()
+
+    if existing:
+        return {"message": "Already in favorites"}
+
+    new_fav = Favorites(pokemon_id=fav.pokemon_id)
+    db.add(new_fav)
+    db.commit()
+
+    return {"message": "Added to favorites"}
+
+# Api to remove a pokemon from the favorites
+@app.delete("/favorites/{pokemon_id}", response_class=FavoritesSchema)
+def remove_from_favorites(pokemon_id: int, db: Session = Depends(get_db)):
+    favorite = db.query(Favorites).filter(Favorites.pokemon_id == pokemon_id).first()
+    if not favorite:
+        raise HTTPException(status_code=404, detail="Favorite not found")
+    db.delete(favorite)
+    db.commit()
+    return {"message": "Removed from favorites"}
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 
 from database import Base
 
@@ -7,3 +7,9 @@ class Pokemon(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index = True)
     sprite_url = Column(String)
+
+class Favorites(Base): 
+    __tablename__ = "favorites"
+    id = Column(Integer, primary_key = True, index = True)
+    pokemon_id = Column(Integer, ForeignKey("pokemon.id"), unique=True)
+
